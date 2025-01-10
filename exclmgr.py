516f579c3043a165befced746647ee9e8e5695c4
raymond <raymondwei@outlook.com>
2025-01-09 19:57:35 -0600

    2025-01-09->19.345 - exclmgr.py/EXclMgr.dic:{<kn>:<NodeManager>} no more use class EXcl

--- exclmgr.py
+++ exclmgr.py
@@ -1,43 +1,25 @@
 from utils.cvsnodetools import *
-
-class Excl:
-    def __init__(self, node=None):
-        self.nodes = []
-        if node:
-            self.add(node)
-
-    def clone(self):
-        nexcl = Excl()
-        nexcl.nodes = copy.deepcopy(self.nodes)
-        return nexcl
-
-    def add(self, node):
-        if type(node) == list:
-            for nd in node:
-                self.add(nd)
-        else:
-            seq = node_seq(node)
-            while not seq.done:
-                nt = seq.get_next()
-                if nt not in self.nodes:
-                    self.nodes.append(nt)
+from nodemanager import NodeManager
 
 class ExclMgr:
     def __init__(self, repo):
         self.repo = repo
-        self.dic = {} # {kn: Excl}
-        self.excls = []
+        # dic: {kn: NodeManager} - for a vk/kn, these nodes are locations
+        # where this vk shouldn't be used(to be excluded)
+        self.dic = {} 
+        self.excluded_vkns = []
 
     def clone(self, repo):
         nexclmgr = ExclMgr(repo)
         nexclmgr.dic = {kn: excl.clone() for kn, excl in self.dic.items()}
+        nexclmgr.excluded_vkns = self.excluded_vkns[:]
         return nexclmgr
     
     def add(self, vkname, node):
         if node == None: 
             self.dic.pop(vkname, None)
-            if vkname not in self.excls:
-                self.excls.append(vkname)
+            if vkname not in self.excluded_vkns:
+                self.excluded_vkns.append(vkname)
             return
-        if vkname in self.excls: return
-        self.dic.setdefault(vkname, Excl()).add(node)
+        # if vkname in self.excls: return #??
+        self.dic.setdefault(vkname, NodeManager(self.repo)).add_node(node)
